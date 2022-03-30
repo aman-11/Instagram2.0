53ddbacc255de23813a8cc8836e845ae7e9370a1
Aayush <aayushaman11@gmail..com>
2022-03-30 14:13:51 +0530

    added comment functionality

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import { useState, useEffect } from "react";
 import {
   BookmarkIcon,
   ChatIcon,
@@ -9,8 +9,50 @@ import {
   EmojiHappyIcon,
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
+import { db, storage } from "../firebase";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import moment from "moment";
 
 function Post({ id, username, userImg, img, caption }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [listComments, setListComments] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setListComments(snapshot.docs)
+      ),
+    [db]
+  );
+
+  const sendComment = (e) => {
+    e.preventDefault();
+
+    const commentToSend = comment;
+    setComment("");
+
+    //firestore
+    addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
   return (
     <div className="bg-white my-7 border rounded-sm">
       {/** header */}
@@ -28,14 +70,17 @@ function Post({ id, username, userImg, img, caption }) {
       <img src={img} alt="" className="object-cover w-full" />
 
       {/** buttons */}
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
+
       {/** captions */}
       <div className="p-5 truncate">
         <p>
@@ -45,17 +90,48 @@ function Post({ id, username, userImg, img, caption }) {
       </div>
 
       {/** comment */}
+      {listComments.length > 0 && (
+        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin ">
+          {listComments.map((comment) => (
+            <div key={comment.id} className="flex items-center space-x-2 mb-3">
+              <img
+                className="h-7 rounded-full"
+                src={comment.data().userImage}
+                alt=""
+              />
+              <p className="text-sm flex-1">
+                <span className="font-bold">{comment.data().username} </span>
+                {comment.data().comment}
+              </p>
+              <p className="text-xs pr-5">
+                {moment(comment.data().timestamp?.toDate()).fromNow()}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/** input box for comment */}
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7 cursor-pointer" />
-        <input
-          className="border-none flex-1 focus:ring-0"
-          placeholder="Add a comment..."
-          type="text"
-        />
-        <button className="font-semibold text-blue-500">Post</button>
-      </form>
+      {session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7 cursor-pointer" />
+          <input
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            className="border-none flex-1 focus:ring-0"
+            placeholder="Add a comment..."
+            type="text"
+          />
+          <button
+            type="submit"
+            disabled={!comment.trim()}
+            onClick={sendComment}
+            className="font-semibold text-blue-500"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -3,29 +3,6 @@ import { useState, useEffect } from "react";
 import { db, storage } from "../firebase";
 import { onSnapshot, collection, query, orderBy } from "firebase/firestore";
 
-// const posts = [
-//   {
-//     id: "123",
-//     username: "aa_yush",
-//     userImg: "https://links.papareact.com/3ke",
-//     img: "https://links.papareact.com/3ke",
-//     caption: "this is going a crazy ride!!",
-//   },
-//   {
-//     id: "124",
-//     username: "xxx_dsd",
-//     userImg: "https://links.papareact.com/3ke",
-//     img: "https://links.papareact.com/3ke",
-//     caption: "this is going a crazy ride!!",
-//   },
-//   {
-//     id: "125",
-//     username: "dasasd",
-//     userImg: "https://links.papareact.com/3ke",
-//     img: "https://links.papareact.com/3ke",
-//     caption: "this is going a crazy ride!!",
-//   },
-// ];
 
 function Posts() {
   const [posts, setPosts] = useState();
